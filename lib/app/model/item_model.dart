1994c941c96da58c8a1334891da3b2a7e60c792e
gauger <gustavojosiasg@gmail.com>
2026-02-23 18:57:09 -0300

    refactor: item_model adicionado campos de quantidade e unidade

--- lib/app/model/item_model.dart
+++ lib/app/model/item_model.dart
@@ -2,12 +2,16 @@ class ItemModel {
   final String id;
   final String nomeItem;
   final double preco;
+  final double quantidade;
+  final String unidade;
   bool isComprado;
 
   ItemModel({
     required this.id,
     required this.nomeItem,
     this.preco = 0.0,
+    this.quantidade = 1.0,
+    this.unidade = 'un',
     this.isComprado = false,
   });
 

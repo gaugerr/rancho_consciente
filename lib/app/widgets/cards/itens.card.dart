55ea8c5f7272a4e1f8e41deb2df52297fa38fecc
gauger <gustavojosiasg@gmail.com>
2026-02-25 20:21:58 -0300

    feat: mudança pra StatefulWidget, initialValue substituido por controllers com valores predefinidos no InitState + melhorias de UX

--- lib/app/widgets/cards/itens.card.dart
+++ lib/app/widgets/cards/itens.card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rancho_consciente/app/model/item_model.dart';
 import 'package:rancho_consciente/app/view_model/rancho_viewmodel.dart';
 
-class ItemCard extends StatelessWidget {
+class ItemCard extends StatefulWidget {
   final RanchoViewModel ranchoViewModel;
   final ItemModel itemModel;
   const ItemCard({
@@ -10,6 +10,37 @@ class ItemCard extends StatelessWidget {
     required this.itemModel,
     required this.ranchoViewModel,
   });
+
+  @override
+  State<ItemCard> createState() => _ItemCardState();
+}
+
+class _ItemCardState extends State<ItemCard> {
+  //key pra o Form() pra usar a validação
+  final _formKey = GlobalKey<FormState>();
+
+  // Controladores para acessar o texto digitado
+  final TextEditingController _qtdController = TextEditingController();
+  final TextEditingController _precoController = TextEditingController();
+
+  // Variável para controlar o estado de carregamento/processamento do botão
+  //final bool _isProcessing = false;
+
+  @override
+  void dispose() {
+    //descartar controladores pra evitar memory leak
+    _qtdController.dispose();
+    _precoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Inicializamos os controllers com os valores que já existem no Model
+    _qtdController.text = widget.itemModel.quantidade.toString();
+    _precoController.text = widget.itemModel.preco.toStringAsFixed(2);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,81 +54,100 @@ class ItemCard extends StatelessWidget {
               // mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Checkbox(
-                  value: itemModel.isComprado,
+                  value: widget.itemModel.isComprado,
                   onChanged: (bool? value) => {
-                    ranchoViewModel.toggleIsComprado(itemModel),
+                    widget.ranchoViewModel.toggleIsComprado(widget.itemModel),
                   },
                 ),
 
                 Text(
-                  itemModel.nomeItem,
+                  widget.itemModel.nomeItem,
                   style: TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
-                    decoration: itemModel.isComprado
+                    decoration: widget.itemModel.isComprado
                         ? TextDecoration.lineThrough
                         : TextDecoration.none,
                   ),
                 ),
               ],
             ),
-            Row(
-              children: [
-                if (itemModel.isComprado) ...[
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: DropdownButtonFormField<String>(
-                      value: itemModel.unidade, // "un", "kg", ou "l"
-                      decoration: const InputDecoration(
-                        labelText: 'Unidade',
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 8,
-                          vertical: 4,
+            Form(
+              key: _formKey,
+              child: Row(
+                children: [
+                  if (widget.itemModel.isComprado) ...[
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: DropdownButtonFormField<String>(
+                        value: widget.itemModel.unidade, // "un", "kg", ou "l"
+                        decoration: const InputDecoration(
+                          labelText: 'Unidade',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4,
+                          ),
                         ),
-                      ),
-                      items: ['un', 'kg', 'L'].map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (novoValor) {
-                        // Aqui você chama sua ViewModel para atualizar o modelo
-                        //ranchoViewModel.atualizarUnidade(itemModel, novoValor!);
-                      },
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: TextFormField(
-                      initialValue: itemModel.quantidade.toString(),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        label: Text('Quantidade'),
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 8,
-                          vertical: 4,
-                        ),
+                        items: ['un', 'kg', 'L'].map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (novoValor) {},
                       ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: TextFormField(
-                      initialValue: itemModel.preco.toString(),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        label: Text('Preço'),
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 8,
-                          vertical: 4,
+                    Expanded(
+                      flex: 1,
+                      child: TextFormField(
+                        keyboardType: const TextInputType.numberWithOptions(
+                          decimal: true,
                         ),
+                        controller: _qtdController,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          label: Text('Quantidade'),
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4,
+                          ),
+                        ),
+                        onTap: () {
+                          _qtdController.selection = TextSelection(
+                            baseOffset: 0,
+                            extentOffset: _qtdController.text.length,
+                          );
+                        },
                       ),
                     ),
-                  ),
+                    Expanded(
+                      flex: 1,
+                      child: TextFormField(
+                        controller: _precoController,
+                        keyboardType: const TextInputType.numberWithOptions(
+                          decimal: true,
+                        ),
+
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          label: Text('Preço'),
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4,
+                          ),
+                        ),
+                        onTap: () {
+                          _precoController.selection = TextSelection(
+                            baseOffset: 0,
+                            extentOffset: _precoController.text.length,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ],
-              ],
+              ),
             ),
           ],
         ),
